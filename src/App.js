f133adaf5584df0bc4c5f2a88cb9d4f32cfe9f83
Clay Grumieaux <claygrumeo@gmail.com>
2022-11-21 15:32:25 -0500

    Add about me section and misc

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import logo from './logo.svg';
 import './App.css';
 import Header from './components/Header/Header';
+import AboutMe from './components/AboutMe/AboutMe';
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <AboutMe />
     </div>
   );
 }
