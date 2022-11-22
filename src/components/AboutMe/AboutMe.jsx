aaec815e300d59d2f6581e53d321623c36e2af42
Clay Grumieaux <claygrumeo@gmail.com>
2022-11-21 21:36:57 -0500

    about me edits

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -8,7 +8,7 @@ export default function AboutMe() {
         <h1 className='AboutMeHeadingText'>About Me</h1>
         <p className='AboutMeText'>
           My name is Clay Grumieaux. I'm a Front-end Developer located in Bloomington, IN. I love writing code and learning
-          new things.  I am currently seeking work as a full time developer.  Click below to download a copy of my resume.
+          new things.  I am currently seeking work as a full time developer in Bloomington or on a remote basis.  Click below to download a copy of my resume.
         </p>
         <a target='_blank' href='https://drive.google.com/file/d/1dG7Gg-ClAgUPKp0rkkgPimxmrviB2owl/view?usp=share_link' className='ResumeButton' download='ClayGrumieauxResume.pdf'>Download Resume</a>
       </div>
