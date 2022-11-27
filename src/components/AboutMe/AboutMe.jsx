34a79f4ce9c38113e18113ca64d3888e102ba815
Clay Grumieaux <claygrumeo@gmail.com>
2022-11-27 14:59:52 -0500

    social link + styling

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,5 +1,6 @@
 import "./AboutMe.css";
 import profilePicture from "../../picture.PNG";
+import linkedInLogo from "../../LI-In-Bug.png";
 
 export default function AboutMe() {
   return (
@@ -19,15 +20,24 @@ export default function AboutMe() {
             <a href="mailto:claygrumeo@gmail.com">claygrumeo@gmail.com</a>.
             Thank you for visiting!
           </p>
-          <a
-            target="_blank"
-            href="https://drive.google.com/file/d/1dG7Gg-ClAgUPKp0rkkgPimxmrviB2owl/view?usp=share_link"
-            className="ResumeButton"
-            download="ClayGrumieauxResume.pdf"
-            rel="noreferrer"
-          >
-            Download Resume
-          </a>
+          <div className="AboutMeButtonsContainer">
+            <a
+              target="_blank"
+              href="https://drive.google.com/file/d/1dG7Gg-ClAgUPKp0rkkgPimxmrviB2owl/view?usp=share_link"
+              className="ResumeButton"
+              download="ClayGrumieauxResume.pdf"
+              rel="noreferrer"
+            >
+              Download Resume
+            </a>
+            <a href="https://www.linkedin.com/in/claygrumeo/" target="_blank" rel="noreferrer">
+              <img
+                className="LinkedInLogo"
+                src={linkedInLogo}
+                alt="linkedin-logo"
+              />
+            </a>
+          </div>
         </div>
       </div>
     </div>
