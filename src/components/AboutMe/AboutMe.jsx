2b6883f22453524aed02769b80b792ec309f6b61
Clay Grumieaux <claygrumeo@gmail.com>
2022-12-14 15:03:10 -0500

    style changes, github, favicon

--- src/components/AboutMe/AboutMe.jsx
+++ src/components/AboutMe/AboutMe.jsx
@@ -1,6 +1,7 @@
 import "./AboutMe.css";
 import profilePicture from "../../picture.PNG";
 import linkedInLogo from "../../LI-In-Bug.png";
+import gitHubLogo from "../../github-mark-white.png";
 
 export default function AboutMe() {
   return (
@@ -12,7 +13,7 @@ export default function AboutMe() {
         <div className="AboutMeContent">
           <h1 className="AboutMeHeadingText">About Me</h1>
           <p className="AboutMeText">
-            My name is Clay Grumieaux (GRU-me-oh). I'm a Front-end Developer
+            My name is Clay Grumieaux (GRU-me-oh). I'm a Software Developer
             located in Bloomington, IN. I love writing code and learning new
             things. Currently, I'm seeking a full-time development role in
             Bloomington or on a remote basis. Click below to download a copy of
@@ -32,9 +33,16 @@ export default function AboutMe() {
             </a>
             <a href="https://www.linkedin.com/in/claygrumeo/" target="_blank" rel="noreferrer">
               <img
-                className="LinkedInLogo"
+                className="SocialLogo"
                 src={linkedInLogo}
                 alt="linkedin-logo"
+              />
+            </a>
+            <a href="https://www.github.com/claygrumeo/" target="_blank" rel="noreferrer">
+              <img
+                className="SocialLogo"
+                src={gitHubLogo}
+                alt="github-logo"
               />
             </a>
           </div>
