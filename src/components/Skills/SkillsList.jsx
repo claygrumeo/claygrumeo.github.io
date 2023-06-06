9e12bf0e90859a0205143eab5ee8a9091a32e6b4
Clay Grumieaux <claygrumeo@gmail.com>
2023-06-06 18:50:30 -0400

    fix key error

--- src/components/Skills/SkillsList.jsx
+++ src/components/Skills/SkillsList.jsx
@@ -4,8 +4,8 @@ export default function SkillsList({ data, type, images, links }) {
   return (
     <ul className={`SkillsList ` + type + `List`}>
       {data.map((listItem, i) => (
-        <div className="SkillsListItem">
-          <SkillsListItem key={listItem.title} listItems={listItem} link={links?.length > 0 ? links[i] : null} />
+        <div className="SkillsListItem" key={i}>
+          <SkillsListItem listItems={listItem} link={links?.length > 0 ? links[i] : null} />
           {images !== undefined && <img className="ProjectThumbnail" src={images[i]} alt={`ProjectImage` + i} />}
         </div>
       ))}
